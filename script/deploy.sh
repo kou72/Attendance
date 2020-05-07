c03dc8351dbd274c98c7cf064954adf39255c919
kou72 <genpokopokopoko@gmail.com>
2020-05-07 18:55:16 +0000

    mod circleci config

---
+++ script/deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# move workking directory
+cd ~/Attendance/
+
+# kill running nuxt server
+kill $(lsof -i:3000 -t)
+
+# force pull to github
+git fetch
+git reset --hard origin/master
+
+# start nuxt server
+npm run build
+nohup npm start &
